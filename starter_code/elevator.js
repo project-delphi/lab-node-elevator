5d8eca0b0449443866f39eae21de193e887c9f7d
Ravi Kalia <ravkalia@gmail.com>
2018-02-10 16:38:09 -0600

    iteration 1

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,22 @@
 class Elevator {
   constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+    this.floor      = 0
+    this.MAXFLOOR   = 10
+    this.requests   = []
+    this.interval   = null
+    this.direction  = 'up' 
   }
 
-  start() { }
-  stop() { }
+  start() { this.interval = setInterval(update(), 1000)}
+  stop() { clearInterval(this.interval)}
   update() { }
   _passengersEnter() { }
   _passengersLeave() { }
   floorUp() { }
   floorDown() { }
   call() { }
-  log() { }
+  log() {console.log(`Direction: ${this.direction}| Floor: ${this.floor}`);
+   }
 }
 
 module.exports = Elevator;
